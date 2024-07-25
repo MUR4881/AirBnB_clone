ec52c26c37ac76417824793d4017fb6c5a502947
MUROBII <usmanmurabi@gmail.com>
2024-07-25 22:52:15 +0100

    Implement the 'create' cmd 4 the cmdline/console:
    > create: Creates a new instance of BaseModel, saves it (to the JSON
    file) and prints the id.
    	Ex: $ create BaseModel
     * If the class name is missing, print ** class name missing **
    	(ex: $ create)
    
     * If the class name doesn't exist, print **class doesn't exist **
    	(ex: $ create MyModel)

--- console.py
+++ console.py
@@ -6,8 +6,9 @@
 Custom HBNBCommand class for managing Object Storage
 at
 """
-
+# Importing dependencies
 from cmd import Cmd
+from models.base_model import BaseModel
 
 
 class HBNBCommand(Cmd):
@@ -15,6 +16,27 @@ class HBNBCommand(Cmd):
     and passing arguments to those funtions
     """
     prompt = "(hbnb) "
+
+    def do_create(self, klass):
+        """Create a new instance of a give class
+        and saves it to the JSON file
+
+        Args:
+            klass: The name of the class as a string
+        """
+        # create the instance, remember the storage automatically reload
+        # Reload, as in read everything stored in the file
+        # And reload as been called in __init__.py
+        try:
+            new_obj = eval(klass)()  #: eval convert the string to the class
+        except SyntaxError:  # eval throws syntax error on empty string
+            # which means class name wasn't passed
+            print("** class name missing **")
+        except NameError:  # This implies that our code attempted to ->
+            print("** class doesn't exist **")  # -> call eval returned Name()
+        else:  # No error thrown, object created!
+            print(new_obj.id)  # Printing the object
+            new_obj.save()  # calling obj.save, which calls storage.save()
 
     def emptyline(self):
         "Method to return nothing if an empty line is entered"
