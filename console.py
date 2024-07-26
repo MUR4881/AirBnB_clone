2b61e0219d744584288c7362c9372cd94b3428af
MUROBII <usmanmurabi@gmail.com>
2024-07-26 12:56:02 +0100

    Refactor commands Implemention (Make it DRY):
    Command Implementations Refactored:
    
    	destroy: refactored with get_instance_by_id
    	show:    refactored with get_instance_by_id
    	also; Error messages, were refracted

--- console.py
+++ console.py
@@ -32,9 +32,9 @@ class HBNBCommand(Cmd):
             new_obj = eval(klass)()  #: eval convert the string to the class
         except SyntaxError:  # eval throws syntax error on empty string
             # which means class name wasn't passed
-            print("** class name missing **")
+            print(Err1)
         except NameError:  # This implies that our code attempted to ->
-            print("** class doesn't exist **")  # -> call eval returned Name()
+            print(Err2)  # -> call eval returned Name()
         else:  # No error thrown, object created!
             print(new_obj.id)  # Printing the object
             new_obj.save()  # calling obj.save, which calls storage.save()
@@ -46,31 +46,11 @@ class HBNBCommand(Cmd):
         Args:
             arg: string: containing class name and id, both separed by space
         """
-        args = split(arg)  #: list: list of the args
-        length = len(args)  #: int: number of args
-        if length == 0:
-            print("** class name missing **")
-        else:  # since there is either classname and/or id
-            try:
-                klass = eval(args[0])  #: getting class variable
-            except SyntaxError:  #: since the returned value was a number 'id'
-                # although this might not be expected to be handled this way
-                print("** class name missing **")
-            except NameError:
-                print("**class doesn't exist **")
-            else:  #: So the class exist!
-                if length < 2:
-                    print("** instance id missing **")
-                else:
-                    #  Get the key -> <class.id>
-                    key = ".".join(args[:2])  # Joining only 1st 2 args as key
-                    try:
-                        objects = storage.all()  # Ref to all reloaded obj
-                        search_object = objects[key]
-                    except KeyError:
-                        print("** no instance found **")
-                    else:
-                        print(search_object)  #: prints obj str, using __Str__
+        key_object = get_object_by_id(arg)  # Getting key and Object
+        # Prevent  Printing None
+        if key_object is not None:
+            # Extract obj ref, from returned tuple
+            print(key_object[1])  #: prints obj str, using __Str__
 
     def do_destroy(self, arg):
         """Destroys an instance, based
@@ -79,31 +59,12 @@ class HBNBCommand(Cmd):
         Args:
             arg: string: Expects a class name, with id both seperated by space
         """
-        args = split(arg)  #: list: list of the args
-        length = len(args)  #: int: number of args
-        if length == 0:
-            print("** class name missing **")
-        else:  # since there is either classname and/or id
-            try:
-                klass = eval(args[0])  #: getting class variable
-            except SyntaxError:  #: since the returned value was a number 'id'
-                # although this might not be expected to be handled this way
-                print("** class name missing **")
-            except NameError:
-                print("**class doesn't exist **")
-            else:  #: So the class exist!
-                if length < 2:
-                    print("** instance id missing **")
-                else:
-                    #  Get the key -> <class.id>
-                    key = ".".join(args[:2])  # Joining only 1st 2 args as key
-                    try:
-                        objects = storage.all()  # Ref to all reloaded obj
-                        del (objects[key])  # since dicts are passed by ref
-                    except KeyError:
-                        print("** no instance found **")
-                    else:
-                        storage.save()
+        key_object = get_object_by_id(arg)  # Getting key and Object
+        # Preventing KeyError Exception
+        if key_object is not None:
+            all_object = storage.all()  # Getting reference to dict of objs
+            del all_object[key_object[0]]  # Index 0 holds the key
+            storage.save()
 
     def emptyline(self):
         "Method to return nothing if an empty line is entered"
@@ -131,6 +92,49 @@ def split(arg):
     Return: A list of the splitted.
     """
     return arg.split()
+
+
+def get_object_by_id(arg):
+    """Get reference to an instance, based
+    on the class name, and it id.
+
+    Args:
+        arg: string: containing class name and UUID, both separed by space
+
+    Return: Key with Reference/address to the object else None if not found
+    """
+    ids = split(arg)  #: list: contains class_name, UUID, or more
+    length = len(ids)  #: int: number of args
+    if length == 0:
+        print(Err1)
+    else:  # since there is either classname and/or id
+        try:
+            klass = eval(ids[0])  #: getting class variable
+        except SyntaxError:  #: since the returned value was a number 'id'
+            # although this might not be expected to be handled this way
+            print(Err1)
+        except NameError:
+            print(Err2)
+        else:  #: So the class exist!
+            if length < 2:
+                print(Err3)
+            else:
+                #  Get the key -> <class.id>
+                key = ".".join(ids[:2])  # Joining only name and UUID
+                try:
+                    objects = storage.all()  # Ref to all reloaded obj
+                    found_object = objects[key]
+                except KeyError:
+                    print(Err4)
+                else:  # Returning key, and reference to objects
+                    return (key, found_object)
+    return None  # Explicitly, for clarity sake
+
+# ------- Error Messages ------------ #
+Err1 = "** class name  missing **"  #: str: Class name error
+Err2 = "** class doesn't exist **"  #: str: Class definition error
+Err3 = "** instance id missing **"  #: str: Instance Id error
+Err4 = "** no instance found **"    #: str: Instance Not Found error
 
 
 if __name__ == "__main__":
