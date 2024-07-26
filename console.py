8475694a97b04fee9965411989e0b788c49a731d
MUROBII <usmanmurabi@gmail.com>
2024-07-26 01:12:33 +0100

    Implement show cmd, for retrieving instance by id:
    show: Prints the string representation of an instance based on the
    class name and id. Ex: $ show BaseModel 1234-1234-1234.
        * If the class name is missing, print ** class name missing **
    	(ex; $ show)
        * If the class name doesn't exist, print **class name doesn't exit
           ** (ex: $ show MyModel)
        * If the id is missing, print ** instance id missing **
    	(ex: $ show BaseModel)
        * If the instance of the class name doesn't exist for the id:
    	print ** no_instance found ** (ex: $ show BaseModel 121212)
    
    Issues:
    	Code need to be refactored, so many Repeat
    	DRY!

--- console.py
+++ console.py
@@ -8,6 +8,7 @@ at
 """
 # Importing dependencies
 from cmd import Cmd
+from models import storage
 from models.base_model import BaseModel
 
 
@@ -38,6 +39,39 @@ class HBNBCommand(Cmd):
             print(new_obj.id)  # Printing the object
             new_obj.save()  # calling obj.save, which calls storage.save()
 
+    def do_show(self, arg):
+        """Prints the string representation of an instance, based
+        on the class name, and it id.
+
+        Args:
+            arg: string: containing class name and id, both separed by space
+        """
+        args = split(arg)  #: list: list of the args
+        length = len(args)  #: int: number of args
+        if length == 0:
+            print("** class name missing **")
+        else:  # since there is either classname and/or id
+            try:
+                klass = eval(args[0])  #: getting class variable
+            except SyntaxError:  #: since the returned value was a number 'id'
+                # although this might not be expected to be handled this way
+                print("** class name missing **")
+            except NameError:
+                print("**class doesn't exist **")
+            else:  #: So the class exist!
+                if length < 2:
+                    print("** instance id missing **")
+                else:
+                    #  Get the key -> <class.id>
+                    key = ".".join(args[:2])  # Joining only 1st 2 args as key
+                    try:
+                        objects = storage.all()  # Ref to all reloaded obj
+                        search_object = objects[key]
+                    except KeyError:
+                        print("** no instance found **")
+                    else:
+                        print(search_object)  #: prints obj str, using __Str__
+
     def emptyline(self):
         "Method to return nothing if an empty line is entered"
         pass
@@ -50,6 +84,20 @@ class HBNBCommand(Cmd):
         "Exit command program with a new line"
         print()
         return True
+
+# ----- Complementary/Supplementary Functions --------- #
+
+
+def split(arg):
+    """Split arguments into list of args
+    by using space as the defaul separator
+
+    Args:
+        arg: The passed arguments to be separated
+
+    Return: A list of the splitted.
+    """
+    return arg.split()
 
 
 if __name__ == "__main__":
