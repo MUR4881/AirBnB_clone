45ccf50234d49ea171961e66f4d78ceb2b0636f5
Aliyu Habibu <aliyuhabibu111@gmail.com>
2025-05-23 22:21:10 +0100

    Inherit TestBaseModel Class as TestAmenity Class:
    Inherit TestBaseModel(test units for base model) as TestAmenity(Amenity
    test units).
    
    Imported the test_base_model (module) to directly (not the
    TestBaseClass) in order to avoid double test repetition.
    
    the module level variable Model (is modified from the module attributes)
    in order to ensure, the test is executed for the the child class.
    
    These 2 processes would be repeated for the other classes:
    place, city, user, e.t.c
    
    Questions:
    
    why does the print function on line 52 print "base model", despite
    overwriting the Model Global variable, in the module of the inheriting
    test class(es) ?

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,28 @@
+# -*- encoding: utf-8 -*-
+
+"""
+Implementation of test class for class Amenity.
+
+The class Amenity:
+    Amenity (models/amenity.py):
+
+    Public class attributes:
+        name: string - empty string
+
+"""
+
+from models.amenity import Amenity
+from tests.test_models import test_base_model
+
+# Changing the Modules global variable, called Model, which is used by
+# the Test Classes so the test can be performed on our desired classes
+
+test_base_model.__dict__["Model"] = Amenity
+
+#print(TestBaseModel.__dict__)
+class TestAmenity(test_base_model.TestBaseModel):
+    """
+        Importing from TestBaseModel, which include the basic test
+        cases and also improving the Amenity test cases
+    """
+
