da3d43c94cb421e67f6d7f98c5c8b3b631d5b365
Aliyu Habibu <aliyuhabibu111@gmail.com>
2025-05-24 00:30:33 +0100

    Unit Test Class State:
    Unit testing the class State by inherit from TestBaseModel Class
    and also implementing specific unit tests.

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+"""
+    Implementing Test Class for State Class:
+
+The State Class:
+    (models/state.py):
+
+    Public class attributes:
+        name: string - empty string
+"""
+
+#Import Dependencies
+from models.state import State
+from . import test_base_model
+
+test_base_model.__dict__["Model"] = State
+
+class TestState(test_base_model.TestBaseModel):
+    """
+    Unit testing State Class by writtting unit
+    test cases and test suite for each edge case
+    """
