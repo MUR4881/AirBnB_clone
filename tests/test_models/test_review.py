19a604a527751d6d96c5df225bbc4631c6d97596
Aliyu Habibu <aliyuhabibu111@gmail.com>
2025-05-24 00:17:55 +0100

    Inherit TestBaseModel to TestReview:
    Inherit the class TestBaseModel, a Test class for BaseModel
    to TestReview, the Review Class test Class.

---
+++ tests/test_models/test_review.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+"""
+    Implement Test Class For Class Review
+The Class Review:
+     (models/review.py):
+
+    Public class attributes:
+        place_id: string - empty string: it will be the Place.id
+        user_id: string - empty string: it will be the User.id
+        text: string - empty string
+"""
+
+#Import Dependencies
+from models.review import Review
+from . import test_base_model
+
+test_base_model.__dict__["Model"] = Review
+
+class TestReview(test_base_model.TestBaseModel):
+    """
+    Unit test the Review Class by implementing
+    test cases and test suite
+    """
