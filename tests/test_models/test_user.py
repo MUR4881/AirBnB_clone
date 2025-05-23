3f78f6c9e8c10e36e09f3ced05809da5bdd3af5c
Aliyu Habibu <aliyuhabibu111@gmail.com>
2025-05-23 22:53:25 +0100

    Inherit BaseModel Test Class to TestUser Class:
    Implemented TestUser Class, a Test Class for User class which
    inherit from TestBaseModel a Test Class for BaseModel.

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,24 @@
+#!/bin/python3
+
+# -*- coding: utf-8 -*-
+
+"""
+    Implementing Test Class for Class User.
+The class User:
+
+    models/user.py
+    Public class attributes:
+        email: string - empty string
+        password: string - empty string
+        first_name: string - empty string
+        last_name: string - empty string
+
+"""
+
+# Importing dependencies
+from models.user import User
+from . import test_base_model
+
+class TestUser(test_base_model.TestBaseModel):
+    """
+    """
